1dd4af594e9a5f664e5654fb1044fd370eb333b4
TeaDrivenDev <TeaDrivenDev@gmail.com>
2018-01-22 02:02:52 +0100

    Implement minimal application loop

--- GjallarhornTest2/Program.fs
+++ GjallarhornTest2/Program.fs
@@ -1,7 +1,57 @@
-﻿namespace GjallarhornTest2X
+﻿namespace GjallarhornTest2
+
+open System
+
+open FsXaml
+open Gjallarhorn
+open Gjallarhorn.Bindable
+open Gjallarhorn.Bindable.Framework
+open Gjallarhorn.Wpf
+
+type MainWindow = XAML<"MainWindow.xaml">
+
 
 module Program =
 
+    let reverse (s : string) = s |> Seq.rev |> String.Concat
+
+    type Msg =
+        | Input of string
+
+    type Thing = { Input : string }
+    with
+        static member Default = { Input = "" }
+    
+    let update msg (thing : Thing) =
+        match msg with
+        | Input input -> { thing with Input = input }
+
+    [<CLIMutable>]
+    type ViewModel = { Original : string; Reverse : string }
+
+    let d = { Original = ""; Reverse = "a" }
+
+    
+    let bindToSource _nav source (model : ISignal<Thing>) : IObservable<Msg> list =
+        let input =
+            model
+            |> Signal.map (fun m -> m.Input)
+            |> Bind.Explicit.twoWay source (nameof <@ d.Original @>)
+
+        model
+        |> Signal.map (fun m -> reverse m.Input)
+        |> Bind.Explicit.oneWay source (nameof <@ d.Reverse @>)
+
+        [
+            Signal.map id input
+            |> Observable.map Input
+        ]
+
+    let applicationCore = Framework.application Thing.Default update (Component.fromExplicit bindToSource) Nav.empty
+
+    [<STAThread>]
     [<EntryPoint>]
     let main _ =
-        0
+        Framework.RunApplication (Navigation.singleViewFromWindow MainWindow, applicationCore)
+
+        1
