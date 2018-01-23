b5fd12b29e9e8950f2db9e9b0af88b881cc885cc
TeaDrivenDev <TeaDrivenDev@gmail.com>
2018-01-23 02:37:39 +0100

    Add button and list

--- GjallarhornTest2/Program.fs
+++ GjallarhornTest2/Program.fs
@@ -17,19 +17,31 @@ module Program =
 
     type Msg =
         | Input of string
+        | Add of string
 
-    type Thing = { Input : string }
+    type Thing =
+        {
+            Input : string
+            Items : string list
+        }
     with
-        static member Default = { Input = "" }
+        static member Default = { Input = ""; Items = [] }
     
     let update msg (thing : Thing) =
         match msg with
         | Input input -> { thing with Input = input }
+        | Add item -> { thing with Input = ""; Items = item :: thing.Items }
 
     [<CLIMutable>]
-    type ViewModel = { Original : string; Reverse : string }
+    type ViewModel =
+        {
+            Original : string
+            Reverse : string
+            Add : VmCmd<string>
+            Items : string list
+        }
 
-    let d = { Original = ""; Reverse = "a" }
+    let d = { Original = ""; Reverse = ""; Add = Vm.cmd ""; Items = [] }
 
     
     let bindToSource _nav source (model : ISignal<Thing>) : IObservable<Msg> list =
@@ -42,9 +54,15 @@ module Program =
         |> Signal.map (fun m -> reverse m.Input)
         |> Bind.Explicit.oneWay source (nameof <@ d.Reverse @>)
 
+        model
+        |> Signal.map (fun m -> m.Items)
+        |> Bind.Explicit.oneWay source (nameof <@ d.Items @>)
+
+        let add = Bind.Explicit.createCommand (nameof <@ d.Add @>) source
+
         [
-            Signal.map id input
-            |> Observable.map Input
+            Signal.map id input |> Observable.map Input
+            add |> Observable.map (fun _ -> model.Value.Input |> reverse |> Add)
         ]
 
     let applicationCore = Framework.application Thing.Default update (Component.fromExplicit bindToSource) Nav.empty
