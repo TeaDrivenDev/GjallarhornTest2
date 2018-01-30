5d70a28c2476a1e4627b08d2dbaf4bdaf0d471a0
TeaDrivenDev <TeaDrivenDev@gmail.com>
2018-01-30 03:32:03 +0100

    Attempt at moving to ViewModels for individual items

--- GjallarhornTest2/Program.fs
+++ GjallarhornTest2/Program.fs
@@ -10,7 +10,6 @@ open Gjallarhorn.Wpf
 
 type MainWindow = XAML<"MainWindow.xaml">
 
-
 module Program =
 
     let names =
@@ -38,51 +37,83 @@ module Program =
            "Pollux"
            "Avior"
         ]
+    
+    type ItemMessage = UpdateItem
 
-    type Msg =
+    [<CLIMutable>]
+    type Item =
+        {
+            Name : string
+            Top : float
+            Left : float
+        }
+
+    let createItem name = { Name = name; Top = 200.; Left = 200. }
+
+    [<CLIMutable>]
+    type ItemViewModel =
+        {
+            XName : string
+            Top : float
+            Left : float
+            Self : ISignal<Item>
+        }
+
+    let designSignal = createItem "" |> Signal.constant
+    let itemd = { XName = ""; Top = 0.; Left = 0.; Self = designSignal }
+
+    let itemComponent =
+        let update msg itemModel =
+            match msg with
+            | UpdateItem -> itemModel
+
+        Component.create<Item, unit, _>
+            [
+                <@ itemd.XName @> |> Bind.oneWay (fun item -> item.Name)
+                <@ itemd.Top @> |> Bind.oneWay (fun item -> item.Top)
+                <@ itemd.Left @> |> Bind.oneWay (fun item -> item.Left)
+                <@ itemd.Self @> |> Bind.self
+            ]
+        |> Component.toSelfUpdating update
+
+    type ModelMessage =
         | Add of string
 
-    type Thing =
+    type Model =
         {
             Source : string list
-            Target : string list
+            Target : Item list
         }
     with
         static member Default = { Source = names; Target = [] }
-    
-    let update msg (thing : Thing) =
+
+    let updateModel msg (model : Model) =
         match msg with
-        | Add item ->
-            if thing.Target |> List.contains item
-            then thing
-            else { thing with Target = (item :: thing.Target) |> List.sort }
-
-    
-    type ItemViewModel =
-        {
-            Name : string
-            Self : ISignal<string>
-        }
-
+        | Add name ->
+            if model.Target |> List.exists (fun item -> item.Name = name)
+            then model
+            else
+                { model with
+                    Target = (createItem name :: model.Target) |> List.sort }
 
     [<CLIMutable>]
     type ViewModel =
         {
-            Source : string list
+            XSource : string list
             Add : VmCmd<string>
-            Target : string list
+            XTarget : ItemViewModel list
         }
 
-    let d = { Source = []; Add = Vm.cmd ""; Target = [] }
+    let d = { XSource = []; Add = Vm.cmd ""; XTarget = [] }
 
-    let bindToSource _nav source (model : ISignal<Thing>) : IObservable<Msg> list =
+    let bindToSource _nav source (model : ISignal<Model>) : IObservable<ModelMessage> list =
         model
         |> Signal.map (fun m -> m.Source)
-        |> Bind.Explicit.oneWay source (nameof <@ d.Source @>)
+        |> Bind.Explicit.oneWay source (nameof <@ d.XSource @>)
 
         model
         |> Signal.map (fun m -> m.Target)
-        |> Bind.Explicit.oneWay source (nameof <@ d.Target @>)
+        |> Bind.Explicit.oneWay source (nameof <@ d.XTarget @>)
 
         let add = Bind.Explicit.createCommandParam (nameof <@ d.Add @>) source
 
@@ -90,7 +121,7 @@ module Program =
             add |> Observable.map Add
         ]
 
-    let applicationCore = Framework.application Thing.Default update (Component.fromExplicit bindToSource) Nav.empty
+    let applicationCore = Framework.application Model.Default updateModel (Component.fromExplicit bindToSource) Nav.empty
 
     [<STAThread>]
     [<EntryPoint>]
